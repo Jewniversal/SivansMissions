1bb2521e40e7651b81d0af266882cbdae12cf9be
Dan Raymond <danraymond95@gmail.com>
2020-01-27 00:04:39 +0200

    Lol

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://missions:missions123@cluster0-cxhwg.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://dan:dan123@cluster0-yeh7k.mongodb.net/test?retryWrites=true&w=majority'
 }
 
 //'mongodb://127.0.0.1:27017/'
